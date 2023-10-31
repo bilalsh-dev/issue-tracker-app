198f71c80f3d6c1ae1e9451e3eb5a9ba11f4520b
bilalshahid929 <bilalshahid929@gmail.com>
2023-10-31 11:44:36 +0400

    Set up the radix UI

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -18,6 +18,5 @@ export async function POST(request: NextRequest) {
   const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
   });
-  console.log("newIssue", newIssue);
   return NextResponse.json(newIssue, { status: 201 });
 }
