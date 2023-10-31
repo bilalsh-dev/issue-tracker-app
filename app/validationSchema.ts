039ad8864e5d5156fdb4c7dd8f4d71a39f255d6c
bilalshahid929 <bilalshahid929@gmail.com>
2023-10-31 16:19:25 +0400

    Extract the error message component

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,13 @@
 import { z } from "zod";
 
 export const createIssueSchema = z.object({
-  title: z.string().min(1, "Title is required.").max(255),
-  description: z.string().min(1, "Description is required."), // Provide a custom error message
+  title: z
+    .string()
+    .nonempty("Title is required")
+    .min(1, "Title is required.")
+    .max(255),
+  description: z
+    .string()
+    .nonempty("Description is required")
+    .min(1, "Description is required."), // Provide a custom error message
 });
