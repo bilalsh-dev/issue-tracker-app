c14bd55c8c5b10bd7ae99ca499c73897fc120fbd
bilalshahid929 <bilalshahid929@gmail.com>
2023-10-31 11:36:43 +0400

    build an Api for creating an issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+export async function GET(request: NextRequest) {
+  return NextResponse.json("abc", { status: 200 });
+}
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+  console.log("newIssue", newIssue);
+  return NextResponse.json(newIssue, { status: 201 });
+}
